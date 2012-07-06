1dcaa090f336d685afa0c39e55d279ac04ad0f87
Atte Hinkka <atte.hinkka@iki.fi>
2012-07-06 20:15:30 +0300

    Fix receive: none to not send anything

--- middleware.py
+++ middleware.py
@@ -255,7 +255,7 @@ class RoutingMiddleware(Middleware):
         should = None
 
         if receive == "none":
-            should = True
+            should = False
         elif receive == "no_echo":
             if sender is recipient:
                 should = False
@@ -276,10 +276,11 @@ class RoutingMiddleware(Middleware):
                     should = False
             should = True
 
-        if 'subscriptions' == "none":
-            should = False
-        elif 'subscriptions' == "all":
-            should = True
+        if receive:
+            if 'subscriptions' == "none":
+                should = False
+            elif 'subscriptions' == "all":
+                should = True
 
         return should
 
