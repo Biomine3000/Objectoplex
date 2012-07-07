c6b14025d827479599de4accf7ffa8c6b9c2de9d
Atte Hinkka <atte.hinkka@iki.fi>
2012-07-07 18:52:46 +0300

    Make oberst handle unicode correctly and add to-header

--- services/oberst.py
+++ services/oberst.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import urllib2
+from codecs import getreader
 
 from system import BusinessObject
 from service import Service
@@ -9,12 +10,24 @@ class Oberst(Service):
     __service__ = 'oberst'
 
     def handle(self, obj):
-        title = urllib2.urlopen('http://biomine.cs.helsinki.fi/oberstdorf/?plain=true').read().replace('\n', '')
+        self.logger.debug(u"Request {0}".format(obj.metadata))
 
-        return BusinessObject({'event': 'service/reply',
-                               'in-reply-to': obj.id,
-                               'size': len(title),
-                               'type': 'text/plain; charset=utf-8'},
-                              bytearray(title, encoding='utf-8'))
+        try:
+            out = urllib2.urlopen('http://biomine.cs.helsinki.fi/oberstdorf/?plain=true')
+            reader = getreader('utf-8')(out)
+            title = reader.read()
+
+
+            metadata = {'event': 'services/reply',
+                        'in-reply-to': obj.id,
+                        'size': len(title),
+                        'type': 'text/plain; charset=utf-8'}
+
+            if 'route' in obj.metadata:
+                metadata['to'] = obj.metadata['route'][0]
+
+            return BusinessObject(metadata, bytearray(title, encoding='utf-8'))
+        except Exception, e:
+            logger.error(u"{0}".format(e))
 
 service = Oberst
