acf6dc8fb1bd6e68c0160351470c1bb72a42feac
Atte Hinkka <atte.hinkka@iki.fi>
2012-07-08 16:22:25 +0300

    Implement print_readably

--- tv.py
+++ tv.py
@@ -3,10 +3,15 @@
 from __future__ import print_function
 
 import logging
+from sys import stdout
 
 from system import *
 
 logger = logging.getLogger("tv")
+non_readable_keys = frozenset(['route', 'id', 'in-reply-to', 'avoid', 'size',
+                               'event', 'type', 'to', 'routing-id', 'sha1'])
+system_parsed_keys = frozenset(['type', 'event'])
+
 
 def registration_object(client_name, user_name):
     metadata = {
@@ -24,6 +29,54 @@ def list_clients_object():
         'avoid': 'hasselhoffian-recursion'
         }
     return BusinessObject(metadata, None)
+
+def format_readably(obj, file=None, no_payload=False, print_hidden_keys=[]):
+    global non_readable_keys
+    global system_parsed_keys
+    snippets = []
+    hidden_keys = set()
+
+    text = None
+    if no_payload is False and \
+           obj.of_content_type('text') and \
+           'size' in obj.metadata and \
+           obj.metadata['size'] > 0:
+        charset = obj.content_type.metadata.get('charset', 'utf-8')
+        text = unicode(obj.payload.decode(charset))
+
+    for key in sorted(obj.metadata.iterkeys()):
+        if key not in non_readable_keys or key in print_hidden_keys:
+            snippet = u"{0}={1}".format(key,
+                                        json.dumps(obj.metadata[key], ensure_ascii=False))
+            snippets.append(snippet)
+        elif key not in system_parsed_keys and key not in print_hidden_keys:
+            hidden_keys.add(key)
+
+    if obj.content_type is not None:
+        snippets.insert(0, u"{0}={1}".format('type',
+                                             json.dumps(unicode(obj.content_type),
+                                                        ensure_ascii=False)))
+    if obj.event is not None:
+        snippets.insert(0, u"{0}={1}".format('event',
+                                             json.dumps(obj.event, ensure_ascii=False)))
+
+    if len(hidden_keys) > 0:
+        snippets.append(u"_hidden={0}".format(json.dumps(sorted(list(hidden_keys)), ensure_ascii=False)))
+
+    if file is not None:
+        if text is not None:
+            print('; '.join(snippets), file=file, end=': ')
+            print(text, file=file)
+        else:
+            print('; '.join(snippets), file=file)
+    else:
+        if text is not None:
+            return u"{0}: {1}".format('; '.join(snippets), text)
+        else:
+            return '; '.join(snippets)
+
+def print_readably(obj, file=stdout, no_payload=False, print_hidden_keys=[]):
+    format_readably(obj, file=file, no_payload=no_payload, print_hidden_keys=print_hidden_keys)
 
  # - "clients/register" - register a client to the server. possible services
  #      implemented by the client will be registered separately as "services/register" events.
